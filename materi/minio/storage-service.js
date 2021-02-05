c47aeec7bf6ae13f9c60271b486b4d825dac783d
unknown <babdurrahman78@gmail.com>
2021-02-05 22:43:07 +0700

    client

--- materi/minio/storage-service.js
+++ materi/minio/storage-service.js
@@ -13,8 +13,8 @@ const client = new Client({
   endPoint: '127.0.0.1',
   port: 9000,
   useSSL: false,
-  accessKey: 'local-minio',
-  secretKey: 'local-test-secret',
+  accessKey: 'minioadmin',
+  secretKey: 'minioadmin',
 });
 
 function randomFileName(mimetype) {
